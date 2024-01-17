e4bf978da987583ecd67cd3f24787ddc02f72034
Topias Tanskanen <tanskanentopias@gmail.com>
2024-01-17 13:32:25 +0200

    teht1 valmis

---
+++ teht1/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+int game(int maxnum);
+int arvauksienMäärä = 0;
+int maxnum;
+int main()
+{
+
+
+
+    game(40);
+    cout << endl;
+    cout << "OIKEIN!" << endl;
+    cout << "Arvauksien maara oli: " << arvauksienMäärä << endl;
+
+
+    return 0;
+}
+
+
+
+int game(int maxnum){
+
+    srand(time(NULL));
+
+    int luku = 0;
+    int satLuku = rand()%maxnum+1;
+
+
+
+
+
+    while(luku != satLuku){
+        //satLuku = rand()%maxnum+1;
+        arvauksienMäärä++;
+        cout << "Arvaa satunnainen luku valilta 1-" << maxnum << ": ";
+        cin >> luku;
+        cout << endl;
+
+        if(luku>maxnum){
+            cout << "Antamasi luku on liian suuri! " << "Yrita uudestaan!" << endl;
+        }
+
+        if(luku<1){
+            cout << "Antamasi luku on liian pieni! " << "Yrita uudestaan!" << endl;
+        }
+
+
+        if(luku>satLuku && luku<=maxnum){
+
+            cout << "Arvaamasi luku: " << luku << " On suurempi kuin satunnainenluku. " << endl;
+            cout << "Yrita uudestaan!" << endl << endl;
+        }
+
+        if(luku<satLuku && luku>0){
+
+            cout << "Arvaamasi luku: " << luku << " On pienempi kuin satunnainenluku. "<< endl;
+            cout << "Yrita uudestaan!" << endl << endl;
+        }
+
+        else if(luku==satLuku && luku>0 && luku<=maxnum){
+
+            return arvauksienMäärä;
+
+
+        }
+
+
+    }
+
+    return arvauksienMäärä;
+}
